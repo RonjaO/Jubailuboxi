47d4839ad8dc2ed3302b432c0a7133e43e79481e
RonjaO <ronja.oja@gmail.com>
2017-01-05 21:45:29 +0200

    Tietokantayhteys ja jotain pientä

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
+var path = require('path');
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Jubailuboxi' });
 });
 
 module.exports = router;
